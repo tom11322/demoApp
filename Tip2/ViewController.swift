86680fa6a3bfd5b2d4c7b9168657446709bc324e
Wade Li <wadeli@Wades-MacBook-Air.local>
2018-12-14 20:10:33 -0500

    Initial Commit

---
+++ Tip2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tip2
+//
+//  Created by Wade Li on 12/14/18.
+//  Copyright © 2018 Wade Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
